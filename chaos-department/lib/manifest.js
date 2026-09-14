19bcd298f0de0daa2e822f4d51c92b7aa19ba9c0
infochaosdepartment-web <info.chaosdepartment@gmail.com>
2026-09-14 13:06:48 +0200

    Add files via upload

---
+++ chaos-department/lib/manifest.js
@@ -0,0 +1,56 @@
+(function () {
+  "use strict";
+
+  window.__BRAND__ = {
+    name: "Chaos Department",
+    tagline: "Creative direction studio",
+    city: "Madrid",
+
+    contact: {
+      email: "info.chaosdepartment@gmail.com",
+      instagram: "chaos.department",
+      instagramUrl: "https://instagram.com/chaos.department"
+    },
+
+    services: [
+      {
+        num: "01",
+        name: "Creative Direction",
+        short: "Conceptos, campa\u00f1as y universos visuales",
+        body: "Conceptos, campa\u00f1as, universos visuales, moodboards, referencias, narrativa y direcci\u00f3n general de proyectos.",
+        photo: "assets/img/work-map.webp"
+      },
+      {
+        num: "02",
+        name: "Content Production",
+        short: "Rodajes, v\u00eddeo, fashion films y fotograf\u00eda",
+        body: "Rodajes, direcci\u00f3n de v\u00eddeo, contenido para artistas, fashion films, fotograf\u00eda, edici\u00f3n y producci\u00f3n de campa\u00f1as.",
+        photo: "assets/img/rodaje-magenta.webp"
+      },
+      {
+        num: "03",
+        name: "Creative Innovation",
+        short: "IA, coolhunting y formatos nuevos",
+        body: "IA, conceptos experimentales, nuevas narrativas, coolhunting y exploraci\u00f3n de formatos que todav\u00eda no son habituales.",
+        photo: "assets/img/instalacion-tv.webp"
+      },
+      {
+        num: "04",
+        name: "Creative Network",
+        short: "Una red de especialistas para ejecutar",
+        body: "Una red de fot\u00f3grafos, dise\u00f1adores, estilistas, directores de arte, motion designers, desarrolladores y otros especialistas para ejecutar proyectos.",
+        photo: "assets/img/plato-rosa.webp"
+      }
+    ],
+
+    capabilities: [
+      "Campa\u00f1as de lanzamiento de producto",
+      "Conceptos de temporada",
+      "Producci\u00f3n de contenido gastron\u00f3mico",
+      "Direcci\u00f3n de fotograf\u00eda y v\u00eddeo",
+      "Campa\u00f1as con artistas",
+      "Activaciones y experiencias",
+      "Direcci\u00f3n creativa de redes"
+    ]
+  };
+})();
